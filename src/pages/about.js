23bcd452abfd8979ba16540f92f7e41ac6677ecd
jlnko <jlnko@hotmail.fr>
2024-11-05 15:36:37 +0100

    ajout des premiers plugin dont celui pour les images

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import * as React from "react"
 import Layout from "../components/layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 const AboutPage = () => {
   return (
     <Layout pageTitle="About Me">
       <p>Si vous souhaitez me contacter remplissez le formulaire suivant.</p>
+      <StaticImage alt="photo chaussette bébé" src="../images/chaussette.jpg" />
     </Layout>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from "react"
 import Layout from "../components/layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>J'ai commencé ce blog avec gatsby js, le 5 novembre 2024.</p>
+      <StaticImage
+        alt="Photo attention chien méchant"
+        src="https://as2.ftcdn.net/v2/jpg/00/29/80/85/1000_F_29808598_w4fhhM0MN4E3Ga4NRokCYFdWjK02YpbL.jpg"
+      />
     </Layout>
   )
 }
